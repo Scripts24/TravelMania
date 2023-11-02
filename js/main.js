54010e0066568a42065a0633964ccee9686430d7
Patricia Atanasoff <system.informatica365@gmail.com>
2023-11-01 23:06:38 -0300

    marquee text gsap

--- js/main.js
+++ js/main.js
@@ -80,3 +80,44 @@ TweenMax.from(".row p", 7,{
     y:20,
     ease: Expo.easeInOut,
 })
+
+// MARQUEE 
+
+let currentScroll = 0
+let isScrollingDown = true
+let arrows = document.querySelectorAll(".arrow")
+
+let tween = gsap 
+.to(".marquee-part", {
+    xPercent: -100,
+    repeat: -1,
+    duration: 5,
+    ease: "linear"
+})
+.totalProgress(0.5)
+
+gsap.set(".marquee-inner", {
+    xPercent: -50,
+})
+
+window.addEventListener("scroll", function(){
+    if(window.pageYOffset > currentScroll){
+        isScrollingDown = true
+    }else{
+        isScrollingDown = false
+    }
+
+    gsap.to(tween, {
+        timeScale: isScrollingDown ? 1 : -1
+    })
+
+    arrows.forEach((arrow) =>{
+        if(isScrollingDown){
+            arrow.classList.remove('active')
+        }else{
+            arrow.classList.add('active')
+        }
+    })
+
+    currentScroll = window.pageYOffset
+})
